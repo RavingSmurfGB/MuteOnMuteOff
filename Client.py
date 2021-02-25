511d6f565a91975bb28105680945fbbe85a4c943
RavingSmurfGB <josephhutchinson4@gmail.com>
2021-02-25 17:50:35 +0000

    everything but updating the icon done :D

--- Client.py
+++ Client.py
@@ -26,10 +26,10 @@ try: #Loads the vlalue from mute.dat and assigns it to the variable m
     print(contents)
     if contents == "Muted":
         m = "Muted"
-        print(" THIS WAS NOT MUTED")
+        print(" m set to muted")
     elif contents == "Unmuted":
         m = "Unmuted"
-        print(" THIS WAS MUTED")
+        print(" m set to unmuted")
     else:
         m = "Unmuted"
         print("VAIRBLAE WAS CHANGED")
@@ -54,28 +54,36 @@ Muteoff = Image.open(r'C:\Users\Joe\Documents\GitHub\MuteOnMuteOff\Images\Muteof
 
 #This code mutes and mutes the mic
 def Mute():
+    global m
+    print("I GOT TO THE MUTE STAGE")
     os.system('cmd /c "start SoundVolumeView.exe /Mute Scarlett Solo USB "')
     f = open("mute.dat", "w")
     f.write("Muted")
     f.close()
+    m = "Muted"
+    print(m)
 
 
 #This code mutes and un mutes the mic
 def Unmute():
+    global m
+    print("I GOT TO THE UNMUTE STAGE")
     os.system('cmd /c "start SoundVolumeView.exe /Unmute Scarlett Solo USB "')
     f = open("mute.dat", "w")
     f.write("Unmuted")
     f.close()
-
+    m = "Unmuted"
+    print(m)
 
 #This code will choose whether to mute ot not to mute...
 def ToMuteOrNotToMute():
     global m
     if m == "Muted":
+        Unmute()
+
+    elif m == "Unmuted":
         Mute()
 
-    if m == "Unmuted":
-        Unmute()
 
 
 
@@ -85,29 +93,29 @@ def ToMuteOrNotToMute():
 def hotkey():
     while True:
         if keyboard.is_pressed("ctrl") & keyboard.is_pressed("enter"):
+            print("You activated my trap card")
             ToMuteOrNotToMute()
-            print("You pressed buttons")
-            time.sleep(.500)
+            time.sleep(1)
 #////////////////////////////////
 
 
 
-'''
+
 #breakthread = True # this controls wether the while loop in main will run
 t = threading.Thread(target=hotkey, name = "(hi, I'm a thread)") # set's t to equal a variable
 t.start() # this starts the thread
 
-'''
+
+
 
 
 #////////////////////////////////Icon loop///////////////////////////////
 # This code will create an icon in the task bar and set to either the variable (pictue) Muteon or Muteoff
 # This code also will need to be run in a continues loop so threading is needed for any other code to run
-print("hi there lads")
+
 
 while True:
-    print("in the loop")
-    print(m)
+
     if m == "Muted":
         #print("muted")
         icon = pystray.Icon(name="name", icon=Muteon, title="Muted")
