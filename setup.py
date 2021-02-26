3cf224357bb144c54422dc925460be5fb76422f7
RavingSmurfGB <josephhutchinson4@gmail.com>
2021-02-26 20:29:40 +0000

    err stuff

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os, shutil
 
 
 #////////////////////////////////Moving Main Files///////////////////////////////
-source_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\source'
+source_dir = '\\source'
 target_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\dest'
 file_names = os.listdir(source_dir)
 
@@ -22,10 +22,29 @@ print("Setting program to start on boot")
 
 #Get's current username
 username = os.getlogin()
-print(username)
+
 
 #Creates the path to startup, including the current user.
 path = ("C:\\Users\\" + username + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")
 
-print(path)
 
+
+#shutil.copy(os.path.join(source_dir, file_names), target_dir)
+#Moving the shortcut to startup
+source_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\source'
+target_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\dest'
+file_name = os.listdir(source_dir)
+for file_name in file_names:
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
+#///////////////////////////////
+
+
+#////////////////////////////////Adding to start menu///////////////////////////////
+
+#C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+
+source_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\source'
+target_dir = 'C:\\Users\\Joe\\Documents\\GitHub\\MuteOnMuteOff\\dest'
+file_name = os.listdir(source_dir)
+for file_name in file_names:
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
