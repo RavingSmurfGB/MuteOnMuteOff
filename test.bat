46df65ad6543af1a8118aaae87a338888732e174
RavingSmurfGB <josephhutchinson4@gmail.com>
2021-05-10 02:57:58 +0100

    removed excess + image sound control panel

--- test.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-soundget, isMute, MICROPHONE, MUTE
-if isMute = Off
-    toMute = 1
-else
-    toMute=0
-SoundSet, toMute, MICROPHONE, MUTE
-return
--- x_test.py
+++
@@ -1,3 +0,0 @@
-import subprocess, pystray
-
-subprocess.call('cmd /c "start support_files\\SoundVolumeView.exe /mute Scarlett Solo USB "')
